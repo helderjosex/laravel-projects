4acedfa9b5886da9ff7e392029f10cd73ad4a310
Hélder José <helderjosex@gmail.com>
2016-11-21 17:20:33 -0300

    Implementado JWT Auth com TDD

---
+++ estoque/tests/AuthControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+
+class AuthControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        // Sets
+        $data=[
+            'username' => 'admin',
+            'email' => 'admin@email.com',
+            'password' => '123456',
+        ];
+
+        //factory(User::class)->create($data);
+
+        $this->post('auth/login', $data);
+
+        // Asserts
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'username' => 'admin',
+        ]);
+    }
+}
